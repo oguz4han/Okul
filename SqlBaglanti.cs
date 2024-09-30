26f0de22f7f3be31592ac547f7820a6d59dabc68
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-09-30 13:30:24 +0300

    Add project files.

---
+++ SqlBaglanti.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace Okul
+{
+
+    public class SqlBaglanti
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-6LPFTLN\\SQLEXPRESS;Initial Catalog=OkulProjesi;Integrated Security=True;Encrypt=False");
+            baglanti.Open();
+            return baglanti;
+        }
+    }
+}
